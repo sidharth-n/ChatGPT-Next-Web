8f440ef34f7a3b2b1ead6c6d437c03a04b41772e
sidharth-n <sidharthnofficial@gmail.com>
2023-11-21 11:33:02 +0530

    update name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -126,10 +126,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          FutureSkool
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Learn Anything Anywhere
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
